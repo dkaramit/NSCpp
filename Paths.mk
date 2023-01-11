a745fbf4a2dfbf94b7b075b923b96721d915205d
dkaramit <dkaramit@yahoo.com>
2023-01-11 22:07:26 +0000

    removed HP_C.dat

--- Paths.mk
+++ Paths.mk
@@ -1,4 +1,3 @@
 #######################################-data files-#######################################
 # relative path to comoslogical data
 cosmoDat=src/data/eos2020.dat
-# cosmoDat=src/data/HP_C.dat
