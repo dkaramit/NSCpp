476dd2a8f30a82ab43bb6993484b6eddd364c114
dkaramit <dkaramit@yahoo.com>
2023-05-25 11:31:41 +0100

    test

--- src/misc_dir/nsc_paths.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PATHS_HEAD
-#define PATHS_HEAD
-
-constexpr const static auto cosmo_PATH = "/home/dimitronic/Desktop/work/work_PD/NSCpp/src/data/eos2020.dat";
-constexpr const static auto rootDir = "/home/dimitronic/Desktop/work/work_PD/NSCpp"; 
-
-#endif
-
